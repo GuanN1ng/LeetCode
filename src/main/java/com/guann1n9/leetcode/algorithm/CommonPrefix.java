cf7eb47946bc360e81ae86157926f14c4441a7cb
GuanN1ng <guann1n9@gmail.com>
2021-03-05 16:10:05 +0800

    公共前缀

---
+++ src/main/java/com/guann1n9/leetcode/algorithm/CommonPrefix.java
@@ -0,0 +1,46 @@
+package com.guann1n9.leetcode.algorithm;
+
+/**
+ *最长公众前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1：
+ *
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * 示例 2：
+ *
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ */
+public class CommonPrefix {
+
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+        if(strs.length == 1){
+            return strs[0];
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                String e = strs[j];
+                if(e.length() == i || e.charAt(i) != c){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+
+    public static void main(String[] args) {
+        String a = "aaaa";
+        System.out.println(a.substring(0, 0));
+    }
+}
