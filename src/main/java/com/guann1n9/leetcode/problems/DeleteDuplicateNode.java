fbcad7e1443a32782b91e79160a3fe5be680c2b7
GuanN1ng <guann1n9@gmail.com>
2021-03-25 20:01:02 +0800

    删除链表重复节点

---
+++ src/main/java/com/guann1n9/leetcode/problems/DeleteDuplicateNode.java
@@ -0,0 +1,76 @@
+package com.guann1n9.leetcode.problems;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ *存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+ *
+ * 返回同样按升序排列的结果链表。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,3,4,4,5]
+ * 输出：[1,2,5]
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,1,1,2,3]
+ * 输出：[2,3]
+ *
+ */
+public class DeleteDuplicateNode {
+
+    /**
+     * 单链
+     */
+    class ListNode{
+        private int val;
+        private ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    /**
+     * 删除重复节点
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0,head);
+        ListNode cur = dummy;
+
+        while (cur.next!= null && cur.next.next != null){
+            if(cur.next.val == cur.next.next.val){
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x ){
+                    cur.next = cur.next.next;
+                }
+            }else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+
+
+
+}
