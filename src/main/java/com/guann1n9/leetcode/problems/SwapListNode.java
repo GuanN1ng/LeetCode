175e9b14dadbcefe9936c46b1cd39fce05d8b159
GuanN1ng <guann1n9@gmail.com>
2021-03-29 22:25:01 +0800

    链表两两交换

---
+++ src/main/java/com/guann1n9/leetcode/problems/SwapListNode.java
@@ -0,0 +1,79 @@
+package com.guann1n9.leetcode.problems;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 示例 2：
+ *
+ * 输入：head = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：head = [1]
+ * 输出：[1]
+ */
+public class SwapListNode {
+
+    static class ListNode{
+        private int val;
+        private ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0,head);
+        ListNode temp = dummy;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummy.next;
+    }
+
+
+   /* public static ListNode swapPairs2(ListNode head){
+
+    }*/
+
+
+
+
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        a.next = b; b.next=c;c.next = d;
+
+        swapPairs(a);
+
+    }
+}
